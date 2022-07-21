d1e7ac1d10123ffb7ee1737e64de0545f9468b3c
Bisesh <bisesh.sitaula@gmail.com>
2022-07-21 17:16:27 +0100

    Integrated win conditions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,18 +23,41 @@ export class AppComponent implements OnInit{
 
   rollDice() {
     for(let i = 0; i < 10; i++) {
+      if (this.dice[i].isHeld) continue;
       this.dice[i].value = (Math.ceil(Math.random() * 6));
     }
+  }
+
+  areDiceSame() {
+    if (this.dice[0] == null) return false;
+    const firstVal = this.dice[0].value;
+    return !this.dice.filter(d => d.value==firstVal)
+  }
+
+  hasWon(): boolean {
+    const firstVal = this.dice[0].value;
+
+    for (const d of this.dice) {
+      if (!d.isHeld) return false;
+      if (d.value != firstVal) return false;
+    }
+
+    return true;
   }
 
   dieHeldChanged(data: any) {
     const { id } = data;
     const die = this.dice[id];
-    console.log(die);
     this.dice[id] = {
       ...die,
       isHeld: !die.isHeld
     }
+    // console.log(this.won);
+    console.log(this.hasWon())
+    // console.log(this.dice[0] == null);
+    // console.log(this.dice.filter(d => d.value==this.dieVal).length)
+    // console.log(this.dice.filter(d => d.value==this.dieVal).length==10)
+
   }
 
   ngOnInit(): void {
